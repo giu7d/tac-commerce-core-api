c6d36f1a7026644f930672f73598a0facb446c35
Giuseppe Setem <giuseppe7d@gmail.com>
2021-07-14 19:31:20 -0300

    refactor: change test case to e2e

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -2,10 +2,9 @@ import request from 'supertest'
 
 import app from '@shared/infra/http/server'
 
-describe('Test', () => {
+describe('Example Test', () => {
 	it('page should return hello world', async () => {
 		const res = await request(app).get('/')
-
 		expect(res.statusCode).toEqual(200)
 		expect(res.body.message).toEqual('Hello World')
 	})
