b6bc82122e69ad0fdab41c92468cae38e51f372b
Giuseppe Setem <giuseppe7d@gmail.com>
2021-07-14 23:18:13 -0300

    fix: open handles

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
 import '@config/index'
 import '@shared/infra/http/server'
+import app from '@shared/infra/http/server'
+import { useColor } from '@shared/utils/console'
+
+const { ENV = 'development', PORT = 3333 } = process.env
+
+app.listen(PORT, () => {
+	console.log(
+		`       
+    ${useColor('purple')(
+			`
+ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   
+█       █       █   ▄  █ █  █ █  █       █   ▄  █  
+█  ▄▄▄▄▄█    ▄▄▄█  █ █ █ █  █▄█  █    ▄▄▄█  █ █ █  
+█ █▄▄▄▄▄█   █▄▄▄█   █▄▄█▄█       █   █▄▄▄█   █▄▄█▄ 
+█▄▄▄▄▄  █    ▄▄▄█    ▄▄  █       █    ▄▄▄█    ▄▄  █
+ ▄▄▄▄▄█ █   █▄▄▄█   █  █ ██     ██   █▄▄▄█   █  █ █
+█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█ v1.0.0
+      `
+		)}
+      ------------------------------
+    \tRunning in:
+    \t${useColor('green')(ENV.toUpperCase())}
+    \tListening at:
+    \t${useColor('blue')(`http://localhost:${PORT}`)}
+      ------------------------------
+  `
+	)
+})
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
-import { useColor } from '@shared/utils/console'
 import { errorHandler } from './middlewares/errorHandler'
-
-const { ENV = 'development', PORT = 3333 } = process.env
 
 const app = express()
 
@@ -12,29 +9,5 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler)
-
-app.listen(PORT, () => {
-	console.log(
-		`       
-    ${useColor('purple')(
-			`
- ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   
-█       █       █   ▄  █ █  █ █  █       █   ▄  █  
-█  ▄▄▄▄▄█    ▄▄▄█  █ █ █ █  █▄█  █    ▄▄▄█  █ █ █  
-█ █▄▄▄▄▄█   █▄▄▄█   █▄▄█▄█       █   █▄▄▄█   █▄▄█▄ 
-█▄▄▄▄▄  █    ▄▄▄█    ▄▄  █       █    ▄▄▄█    ▄▄  █
- ▄▄▄▄▄█ █   █▄▄▄█   █  █ ██     ██   █▄▄▄█   █  █ █
-█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█  █▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█ v1.0.0
-      `
-		)}
-      ------------------------------
-    \tRunning in:
-    \t${useColor('green')(ENV.toUpperCase())}
-    \tListening at:
-    \t${useColor('blue')(`http://localhost:${PORT}`)}
-      ------------------------------
-  `
-	)
-})
 
 export default app
